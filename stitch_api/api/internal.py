1e25876107aded057e704affff3a76ae58293c88
Ryan McCormack <rmccormack@wealthsimple.com>
2020-04-20 15:10:17 -0400

    clean internal api

--- stitch_api/api/internal.py
+++ stitch_api/api/internal.py
@@ -8,14 +8,14 @@ from .common import BaseStitchApi
 class Source(BaseStitchApi):
 
     @classmethod
-    def reset(cls, source_id, client_id=None) -> namedtuple:
+    def reset(cls, source_id, client_id=None, *args, **kwargs) -> namedtuple:
         url = ('/menagerie/public/v1/clients/{client_id}/connections/'
                '{source_id}/state').format(client_id=client_id,
                                            source_id=source_id)
         return cls.send_request(url, method='delete')
 
     @classmethod
-    def daily_report(cls, client_id=None) -> namedtuple:
+    def daily_report(cls, client_id=None, *args, **kwargs) -> namedtuple:
         url = '/clients/{client_id}/stats/daily'.format(client_id=client_id)
         return cls.send_request(url, method='get')
 
@@ -23,7 +23,7 @@ class Source(BaseStitchApi):
 class Stream(BaseStitchApi):
 
     @classmethod
-    def reset(cls, source_id: int, stream_id: int, client_id: Optional[int] = None) -> namedtuple:
+    def reset(cls, source_id: int, stream_id: int, client_id: Optional[int] = None, *args, **kwargs) -> namedtuple:
         url = ('/menagerie/public/v1/clients/{client_id}/connections/'
                '{source_id}/bookmark/streams/{stream_id}').format(client_id=client_id,
                                                                   source_id=source_id,
@@ -32,12 +32,9 @@ class Stream(BaseStitchApi):
 
     @classmethod
     def get_load_data(cls, source_id: int, stream_name: str,
-                      time_range_start: datetime, time_range_end: datetime,
+                      start_iso: str, end_iso: str,
                       limit: int = 100, offset: int = 0,
-                      client_id: Optional[int] = None) -> namedtuple:
-        # TODO clean up and assert time ranges
-        start_iso = time_range_start.strftime('%Y-%m-%dT%H:%M:%S.%f')[:-3] + 'Z'
-        end_iso = time_range_end.strftime('%Y-%m-%dT%H:%M:%S.%f')[:-3] + 'Z'
+                      client_id: Optional[int] = None, *args, **kwargs) -> namedtuple:
         time_start = url_parse.quote(start_iso)
         time_end = url_parse.quote(end_iso)
         url = ("/clients/{client_id}/connections/{source_id}/loading-reports/tables/{stream_name}?"
